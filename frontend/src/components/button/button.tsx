2622d48ccb363dc71708a8b27e1f5fe0a884c9f6
myagizmaktav <myagizmaktav@gmail.com>
2024-05-27 17:12:35 +0300

    feat: add button comp

---
+++ frontend/src/components/button/button.tsx
@@ -0,0 +1,23 @@
+import styles from "./button.module.scss";
+
+type ButtonProps = {
+  backgroundColor?: string;
+  title: string | JSX.Element;
+  onclick?: () => void;
+};
+
+export const Button = ({
+  backgroundColor = "red",
+  title,
+  onclick = () => {},
+}: ButtonProps) => {
+  return (
+    <button
+      style={{ backgroundColor, border: `1px solid ${backgroundColor}` }}
+      className={styles.button}
+      onClick={onclick}
+    >
+      {title}
+    </button>
+  );
+};
